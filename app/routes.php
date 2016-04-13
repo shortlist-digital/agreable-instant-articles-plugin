4b2cf809e0e65f9f71b87444dfcb2a10ad2ee210
Jon Sherrard <jon@andthats.it>
2016-04-13 16:08:59 +0100

    Test route

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,12 @@
 /** @var \Herbert\Framework\Router $router */
 
 $router->get([
+  'as'   => 'testView',
+  'uri'  => '/instant-articles/test',
+  'uses' => __NAMESPACE__ . '\Controllers\TestController@index'
+]);
+
+$router->get([
   'as'   => 'postView',
   'uri'  => '/{category_slug}/{post_slug}/instant-articles',
   'uses' => __NAMESPACE__ . '\Controllers\PostController@view'
