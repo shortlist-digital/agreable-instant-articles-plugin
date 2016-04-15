9f295967ad36f5b40898ad83dcc0358734c3be17
Jon Sherrard <jon@andthats.it>
2016-04-15 11:22:46 +0100

    More forms for selecting the correct Facebook Page

--- app/Controllers/PanelController.php
+++ app/Controllers/PanelController.php
@@ -11,7 +11,7 @@ class PanelController {
   public $fields = [
     ['name'=> 'instant_articles_app_id', 'label'=> 'Instant Articles App ID', 'description'=> 'The Facebook App ID for your Instant Articles'],
     ['name'=> 'instant_articles_app_secret', 'label'=> 'Instant Articles App Secret', 'description'=> 'The Facebook App secret for your Instant Articles', 'type' => 'password'],
-    ['name'=> 'instant_articles_page_id', 'label'=> 'Instant Articles Page ID', 'description'=> 'The Facebook Page ID to post to Instant Articles']
+    ['name'=> 'instant_articles_page_id', 'label'=> 'Instant Articles Page ID', 'description'=> 'The Facebook Page ID to post to Instant Articles', 'disabled' => true]
   ];
 
   public function index() {
@@ -20,7 +20,8 @@ class PanelController {
     }
     return view('@AgreableInstantArticlesPlugin/admin/index.twig', [
       'fields' => $this->fields,
-      'pages' => $this->pages()
+      'pages' => $this->pages(),
+      'selected' => get_option('instant_articles_page_name')
     ]);
   }
 
@@ -38,12 +39,19 @@ class PanelController {
   }
 
   public function saveConfig(Http $http) {
-    echo "<pre>";print_r($http->all());die;
     foreach($this->fields as $field) {
       update_option($field['name'], $http->get($field['name']));
     }
+    $this->decode_facebook_page_data($http->get('instant_articles_page'));
     $redirect = $_SERVER['HTTP_REFERER'];
     header("location: $redirect");
+  }
+
+  public function decode_facebook_page_data($data_string) {
+    $page_data = json_decode($data_string, true);
+    foreach($page_data as $key => $value) {
+      update_option("instant_articles_page_$key", $value);
+    }
   }
 
   public function clearConfig() {
