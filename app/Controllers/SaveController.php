fdfcc1a09d8484760ef27fe7249b07c3f21c3a18
Jon Sherrard <jon@andthats.it>
2016-04-18 15:09:21 +0100

    Save controller gets HTML

--- app/Controllers/SaveController.php
+++ app/Controllers/SaveController.php
@@ -2,9 +2,28 @@
 
 use TimberPost;
 
+use Facebook\InstantArticles\Elements\InstantArticle;
+use Facebook\InstantArticles\Transformer\Transfomer;
+
 class SaveController {
+
   function __construct(TimberPost $post) {
     $this->post = $post;
-    print_r($this->post);die;
+    $this->instant_article = InstantArticle::create();
+    $permalink = get_permalink($this->post->id);
+    $url = "$permalink/instant-articles";
+    $this->build_article_object($url);
+    print_r($url);die;
   }
+
+  public function build_article_object($url) {
+    try {
+      $html = file_get_contents($url);
+    } catch (\Exception $e) {
+      echo $e->getMessage();
+      die;
+    }
+    print_r($html);die;
+  }
+
 }
