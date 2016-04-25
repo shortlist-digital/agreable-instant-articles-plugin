d8e945257295e5af28bfe71ef3fe1b8d65652e85
ecoad <elliotcoad@gmail.com>
2016-04-25 14:07:36 +0100

    IA DEVELOPMENT_MODE is now via .env

--- app/Services/ClientProvider.php
+++ app/Services/ClientProvider.php
@@ -4,12 +4,20 @@ namespace AgreableInstantArticlesPlugin\Services;
 class ClientProvider {
 
   function __construct() {
+
+    // String is needed for the comparison
+    if (getenv('FB_INSTANT_ARTICLES_DEVELOPMENT_MODE') === "false") {
+      $development_mode = false;
+    } else {
+      $development_mode = true;
+    }
+
     $this->client = Client::create(
       get_option('instant_articles_app_id'),
       get_option('instant_articles_app_secret'),
       get_option('instant_articles_page_token'),
       get_option('instant_articles_page_id'),
-      true// development environment?
+      $development_mode
     );
   }
 
