47325f6cd27849647418d84b63b52b291f2ffa27
Jon Sherrard <jon@andthats.it>
2017-09-28 15:28:51 +0100

    missed a commit

--- app/Generators/Embed.php
+++ app/Generators/Embed.php
@@ -8,17 +8,12 @@ use TimberLoader;
 class Embed implements GeneratorInterface
 {
     public function get( $widget ) {
-<<<<<<< HEAD
-        $widget['caption'] = strip_tags($widget['caption']);
-        $html_as_string = Timber::compile( __DIR__ . '/views/embed.twig', $widget, false, TimberLoader::CACHE_NONE);
-=======
         if ( ! isset( $widget['caption'] ) ) {
             return '';
         }
 
         $widget['caption'] = strip_tags( $widget['caption'] );
         $html_as_string = Timber::compile( __DIR__ . '/views/embed.twig', $widget );
->>>>>>> develop
 
         return $html_as_string;
     }
