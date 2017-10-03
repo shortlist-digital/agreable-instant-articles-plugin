aa2d356812e9a8e9e1863d900699318185a2381a
Romulo De Lazzari <romulodelazzari@gmail.com>
2017-10-03 14:14:03 +0100

    add ads to facebook instant articles generators

--- app/Generators/Heading.php
+++ app/Generators/Heading.php
@@ -2,20 +2,17 @@
 
 namespace AgreableInstantArticlesPlugin\Generators;
 
-use Timber;
-use TimberLoader;
-
 class Heading implements GeneratorInterface
 {
     public function get( $widget ) {
         $text = strip_tags($widget['text']);
         $text = html_entity_decode($text);
-        $html_as_string = Timber::compile(
-          __DIR__ . '/views/heading.twig',
-          array(
-            'text' => $text
-          ),
-          false, TimberLoader::CACHE_NONE
+        $html_as_string = \Timber::compile(
+            __DIR__ . '/views/heading.twig',
+            array(
+                'text' => $text
+            ),
+            false, \TimberLoader::CACHE_NONE
         );
         return $html_as_string;
     }
--- app/Generators/SuperHero.php
+++ app/Generators/SuperHero.php
@@ -2,10 +2,6 @@
 
 namespace AgreableInstantArticlesPlugin\Generators;
 
-use Timber;
-use TimberLoader;
-use TimberSite;
-
 class SuperHero implements GeneratorInterface
 {
     public function get( $post ) {
@@ -24,19 +20,41 @@ class SuperHero implements GeneratorInterface
 
         $category = $post->terms( 'category' );
 
-        return Timber::compile(
+        return \Timber::compile(
             __DIR__ . '/views/super-hero.twig',
             [
-                'site' => new TimberSite(),
+                'site' => new \TimberSite(),
                 'post' => $post,
                 'post_category' => $post->post_category,
                 'post_category_slug' => $category[0]->slug,
                 'post_date' => gmdate('d M Y', strtotime($post->post_date)),
                 'SEGMENT_WRITE_KEY' => getenv( 'SEGMENT_WRITE_KEY' ),
-                'canonical_url' => $url
+                'canonical_url' => $url,
+                'adverts' => $this->get_adverts( $post )
             ],
             false,
-            TimberLoader::CACHE_NONE
+            \TimberLoader::CACHE_NONE
         );
+    }
+
+    private function get_adverts( $post ) {
+        $web_base_url = getenv('WEB_BASE_URL');
+
+        $category = get_the_category($post->ID);
+        $zone = get_home_url();
+
+        if ( isset( $category[0] ) && $category[0] instanceof \WP_Term ) {
+            $zone = $category[0]->slug;
+        }
+
+        $path = str_replace( $web_base_url, '', get_permalink( $post->ID ) );
+
+        return [
+            [
+                'web_base_url' => $web_base_url,
+                'zone'         => $zone,
+                'path'         => $path
+            ]
+        ];
     }
 }
