2f8845f3941a0bac93105f582e04f7ec4e4f0771
Romulo De Lazzari <romulodelazzari@gmail.com>
2018-01-09 11:36:22 +0000

    add button generator that returns an empty string for now

---
+++ app/Generators/Button.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AgreableInstantArticlesPlugin\Generators;
+
+class Button implements GeneratorInterface
+{
+    public function get( $widget = null ) {
+        return "";
+    }
+}
